39164a3414a2ccaf5692e65c461df9cdeeedd462
Gustavo Munoz <softwareadolfo@gmail.com>
2023-01-24 11:06:57 -0600

    Gustavo Munoz:CRUD HECHO CON DAPPER Y ASP.NET CORE 6

---
+++ CRUD DAPPER/CRUD DAPPER/Controllers/ClienteController.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Entities;
+using Logic;
+
+
+namespace CRUD_DAPPER.Controllers
+{
+    public class ClienteController : Controller
+    {
+      LogicClient  logicCliente = new LogicClient();
+
+        // GET: ClienteController
+        public ActionResult Index()
+        {
+            var roles = logicCliente.getAll();
+            return View(roles);
+        }
+
+        // GET: ClienteController/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Obtener el usuario
+
+            var cliente = logicCliente.getId(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(cliente);
+
+
+
+        }
+
+        // GET: ClienteController/Create
+        public ActionResult Create()
+        {
+            return View();
+
+            
+        }
+
+        // POST: ClienteController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cliente cliente)
+        {
+           
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    logicCliente.Create(cliente);
+
+                    TempData["mensaje"] = "El usuario se a creado correctamente";
+                }
+
+               
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(cliente);
+            }
+        }
+
+        // GET: ClienteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Obtener el usuario
+
+            var cliente = logicCliente.getId(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(cliente);
+        
+        }
+
+        // POST: ClienteController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Cliente cliente)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var pCliente = logicCliente.Update(cliente,cliente.Id);
+                    TempData["mensaje"] = "El usuario se ha actualizado correctamente";
+                    return RedirectToAction("Index");
+                }
+
+                
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(cliente);
+            }
+        }
+
+        // GET: ClienteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Obtener el usuario
+
+            var cliente = logicCliente.getId(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(cliente);
+           
+        }
+
+        // POST: ClienteController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCliente(Cliente cliente)
+        {
+            try
+            {
+
+
+                var pCliente = logicCliente.Delete(cliente.Id);
+
+                TempData["mensaje"] = "El libro se ha borrado correctamente";
+           
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Index));
+       
+            }
+        }
+    }
+}
---
+++ CRUD DAPPER/Logic/LogicClient.cs
@@ -0,0 +1,173 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+namespace Logic
+{
+    public class LogicClient
+    {
+        string connectionString = "Data Source=DESKTOP-6H57L2G;Initial Catalog=CRUD_DAPPER;Integrated Security=True";
+
+
+        public Cliente Create(Cliente cliente)
+        {
+            try
+            {
+
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                    Cliente pCliente = new Cliente()
+                    {
+
+                        Nombre = cliente.Nombre,
+                        Apellido = cliente.Apellido,
+                    };
+
+                    string sqlQuery = "INSERT INTO Cliente (Nombre, Apellido) VALUES(@Nombre, @Apellido)";
+
+                    int rowsAffected = db.Execute(sqlQuery, pCliente);
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new ArgumentException("error al insertar a la base de datos");
+
+                    }
+                    return pCliente;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new ArgumentException("error al insertar a la base de datos");
+            }
+        }
+
+        public Cliente Update(Cliente cliente, int Id)
+        {
+            try
+            {
+
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                    Cliente pCliente = new Cliente()
+                    {
+
+                        Nombre = cliente.Nombre,
+                        Apellido = cliente.Apellido,
+                    };
+
+                    string sqlQuery = $"UPDATE Cliente Set Nombre=@Nombre,Apellido=@Apellido where Id={Id}";
+
+
+                    int rowsAffected = db.Execute(sqlQuery, pCliente);
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new ArgumentException("error al actualizar a la base de datos");
+
+                    }
+                    return pCliente;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new ArgumentException("error al actualizar a la base de datos");
+            }
+        }
+
+        public int Delete(int Id)
+        {
+            try
+            {
+
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+                   
+                    string sqlQuery = $"DELETE FROM Cliente WHERE Id={Id}";
+
+
+
+
+
+                    int rowsAffected = db.Execute(sqlQuery);
+
+                    if (rowsAffected == 0)
+                    {
+                        throw new ArgumentException("error al actualizar a la base de datos");
+
+                    }
+                    return rowsAffected;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new ArgumentException("error al actualizar a la base de datos");
+            }
+        }
+
+        public List<Cliente> getAll()
+        {
+            try
+            {
+                List<Cliente> list = new List<Cliente>();
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+
+
+                    string sqlQuery = "select * from Cliente";
+
+                    var Clientes = db.Query<Cliente>(sqlQuery);
+                    foreach (var item in Clientes)
+                    {
+
+
+                        list.Add(item);
+                    }
+                    return list;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw new ArgumentException("error al consultar datos");
+            }
+        }
+
+        public Cliente getId(int Id)
+        {
+
+            try
+            {
+                
+                using (IDbConnection db = new SqlConnection(connectionString))
+                {
+
+
+                    string sqlQuery  = "select * from Cliente";
+
+                    var Clientes = db.QuerySingle<Cliente>(sqlQuery);
+
+                    return Clientes;
+                }
+
+           
+            }
+            catch (Exception e)
+            {
+
+                throw new ArgumentException("error al consultar datos");
+            }
+        }
+    }
+}
